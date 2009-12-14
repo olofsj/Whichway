f7ff6e507f71a85da66d4bcba8bbac2c3c6eb825
olofsj <olofsj@gmail.com>
2009-12-14 19:20:44 +0100

    Parse all ways and get their coordinates from the nodes.

--- src/bin/whichway_create_index.c
+++ src/bin/whichway_create_index.c
@@ -11,8 +11,10 @@
 #include "whichway_internal.h"
 
 typedef struct _File File;
-typedef struct _Node Node;
+typedef struct _WayNode WayNode;
 typedef struct _Way Way;
+typedef struct _NodeList NodeList;
+typedef struct _Node Node;
 
 struct _File {
     char *file;
@@ -21,43 +23,134 @@ struct _File {
     int size;
 };
 
-struct _Node {
+struct _WayNode {
     int id;
-    Node *next;
-    Node *prev;
+    WayNode *next;
+    WayNode *prev;
 };
 
 struct _Way {
-    Node *start;
-    Node *end;
+    WayNode *start;
+    WayNode *end;
     int oneway;
     int size;
+};
+
+struct _NodeList {
+    NodeList *next;
+    Node *node;
+};
+
+struct _Node {
+    int id;
+    double lat;
+    double lon;
 };
 
 /* Global variables */
 int depth;
 int node_count;
+NodeList *node_list;
+Node **nodes;
 int way_count;
 RoutingIndex ri;
 Way way;
 
+
+NodeList *
+nodelist_insert(NodeList *list, NodeList *el) {
+    NodeList *cn;
+
+    if (!list)
+        return el;
+
+    if (el->node->id < list->node->id) {
+        el->next = list;
+        return el;
+    }
+
+    cn = list;
+    while (cn->next) {
+        if (el->node->id < cn->next->node->id) {
+            el->next = cn->next;
+            cn->next = el;
+            return list;
+        }
+        cn = cn->next;
+    }
+
+    cn->next = el;
+    return list;
+}
+
+Node *
+node_find(Node** nodes, int id, int low, int high) {
+    int mid;
+
+    if (high < low)
+        return NULL; // not found
+
+    mid = low + ((high - low) / 2);
+    if (nodes[mid]->id > id) {
+        return node_find(nodes, id, low, mid-1);
+    } else if (nodes[mid]->id < id) {
+        return node_find(nodes, id, mid+1, high);
+    } else {
+        return nodes[mid];
+    }
+}
+
+Node *get_node(int id) {
+    return node_find(nodes, id, 0, node_count-1);
+}
+
+
 void
-start(void *data, const char *el, const char **attr) {
+nodeparser_start(void *data, const char *el, const char **attr) {
   int i;
 
   if (!strcmp(el, "node")) {
+      NodeList *l;
+
       node_count++;
-  }
-  else if (!strcmp(el, "way")) {
-      way_count++;
+
+      l = malloc(sizeof(NodeList));
+      l->next = NULL;
+      l->node = malloc(sizeof(Node));
 
       /* Check all the attributes for this node */
       for (i = 0; attr[i]; i += 2) {
-          way.size = 0;
-          way.start = NULL;
-          way.end = NULL;
-          way.oneway = 0;
+          if (!strcmp(attr[i], "id")) 
+              sscanf(attr[i+1], "%d", &(l->node->id));
+          if (!strcmp(attr[i], "lat")) 
+              sscanf(attr[i+1], "%lf", &(l->node->lat));
+          if (!strcmp(attr[i], "lon")) 
+              sscanf(attr[i+1], "%lf", &(l->node->lon));
       }
+
+      node_list = nodelist_insert(node_list, l);
+  }
+
+  depth++;
+}
+
+void
+nodeparser_end(void *data, const char *el) {
+
+    depth--;
+}
+
+void
+wayparser_start(void *data, const char *el, const char **attr) {
+  int i;
+
+  if (!strcmp(el, "way")) {
+      way_count++;
+
+      way.size = 0;
+      way.start = NULL;
+      way.end = NULL;
+      way.oneway = 0;
   }
   else if (!strcmp(el, "tag") && way.size != -1) {
       if (!strcmp(attr[0], "k") && !strcmp(attr[1], "oneway") &&
@@ -71,12 +164,12 @@ start(void *data, const char *el, const char **attr) {
       // Add a node to the current way
       way.size++;
       if (!way.start) {
-          way.start = malloc(sizeof(Node));
+          way.start = malloc(sizeof(WayNode));
           way.end = way.start;
           way.start->prev = NULL;
           way.start->next = NULL;
       } else {
-          way.end->next = malloc(sizeof(Node));
+          way.end->next = malloc(sizeof(WayNode));
           way.end->next->next = NULL;
           way.end->next->prev = way.end;
           way.end = way.end->next;
@@ -93,9 +186,10 @@ start(void *data, const char *el, const char **attr) {
 }
 
 void
-end(void *data, const char *el) {
+wayparser_end(void *data, const char *el) {
     int i, index;
-    Node *cn;
+    WayNode *cn;
+    Node *nd;
 
     if (!strcmp(el, "way")) {
         if (way.start->id == 302797085) {
@@ -114,19 +208,47 @@ end(void *data, const char *el) {
         while (cn->next) {
             ri.ways[index].from.id = cn->id;
             ri.ways[index].to.id = cn->next->id;
+
+            nd = get_node(ri.ways[index].from.id);
+            if (nd) {
+                ri.ways[index].from.lat = nd->lat;
+                ri.ways[index].from.lon = nd->lon;
+            }
+
+            nd = get_node(ri.ways[index].to.id);
+            if (nd) {
+                ri.ways[index].to.lat = nd->lat;
+                ri.ways[index].to.lon = nd->lon;
+            }
+
             index++;
+
             if (!way.oneway) {
                 ri.ways[index].from.id = cn->next->id;
                 ri.ways[index].to.id = cn->id;
+
+                nd = get_node(ri.ways[index].from.id);
+                if (nd) {
+                    ri.ways[index].from.lat = nd->lat;
+                    ri.ways[index].from.lon = nd->lon;
+                }
+
+                nd = get_node(ri.ways[index].to.id);
+                if (nd) {
+                    ri.ways[index].to.lat = nd->lat;
+                    ri.ways[index].to.lon = nd->lon;
+                }
+
                 index++;
             }
+
             cn = cn->next;
         }
 
         // Free the nodes
         cn = way.start;
         while (cn) {
-            Node *next;
+            WayNode *next;
             next = cn->next;
             free(cn);
             cn = next;
@@ -146,6 +268,9 @@ main(int argc, char **argv)
     struct stat st;
     char *filename;
     int i;
+    int done;
+    int len;
+    NodeList *cn;
     
     
     printf("Whichway Create Index\n");
@@ -182,36 +307,66 @@ main(int argc, char **argv)
     printf("filesize: %d\n", osmfile.size);
 
 
-    /* Set up the XML parser */
-    XML_Parser p = XML_ParserCreate(NULL);
-    if (!p) {
+    // Set up an XML parser for the nodes
+    XML_Parser nodeparser = XML_ParserCreate(NULL);
+    if (!nodeparser) {
         fprintf(stderr, "Couldn't allocate memory for parser\n");
         exit(-1);
     }
 
-    XML_SetElementHandler(p, start, end);
-
-    int done;
-    int len;
+    XML_SetElementHandler(nodeparser, nodeparser_start, nodeparser_end);
 
     depth = 0;
     node_count = 0;
+    node_list = NULL;
+
+    /* Parse the XML document */
+    if (! XML_Parse(nodeparser, osmfile.content, osmfile.size, done)) {
+        fprintf(stderr, "Parse error at line %d:\n%s\n",
+                (int)XML_GetCurrentLineNumber(nodeparser),
+                XML_ErrorString(XML_GetErrorCode(nodeparser)));
+        exit(-1);
+    }
+
+    // Create an indexed sorted list of nodes
+    nodes = malloc(node_count * sizeof(Node *));
+    i = 0;
+    cn = node_list;
+    while (cn) {
+        nodes[i++] = cn->node;
+        cn = cn->next;
+    }
+
+    // Set up an XML parser for the ways
+    XML_Parser wayparser = XML_ParserCreate(NULL);
+    if (!wayparser) {
+        fprintf(stderr, "Couldn't allocate memory for parser\n");
+        exit(-1);
+    }
+
+    XML_SetElementHandler(wayparser, wayparser_start, wayparser_end);
+
+    depth = 0;
     way_count = 0;
     ri.size = 0;
     ri.ways = NULL;
     way.size = -1;
 
     /* Parse the XML document */
-    if (! XML_Parse(p, osmfile.content, osmfile.size, done)) {
+    if (! XML_Parse(wayparser, osmfile.content, osmfile.size, done)) {
         fprintf(stderr, "Parse error at line %d:\n%s\n",
-                (int)XML_GetCurrentLineNumber(p),
-                XML_ErrorString(XML_GetErrorCode(p)));
+                (int)XML_GetCurrentLineNumber(wayparser),
+                XML_ErrorString(XML_GetErrorCode(wayparser)));
         exit(-1);
     }
 
-    printf("Number of nodes: %d\n", ri.size);
+    printf("Number of nodes: %d\n", node_count);
     printf("Number of ways: %d\n", way_count);
     for (i = 0; i < 10; i++)
-        printf("Way %d: (%d %d)\n", i, ri.ways[i].from.id, ri.ways[i].to.id);
+        printf("Way %d: %d (%lf %lf) - %d (%lf %lf)\n", i, 
+                ri.ways[i].from.id, ri.ways[i].from.lat, ri.ways[i].from.lon, 
+                ri.ways[i].to.id, ri.ways[i].to.lat, ri.ways[i].to.lon);
+    //for (i = 0; i < 10; i++)
+        //printf("Node %d: %d (%lf %lf)\n", i, nodes[i]->id, nodes[i]->lat, nodes[i]->lon);
 }
 
